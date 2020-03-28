1ad211421e769699fcbb6b43bbbf6e2bdd00d0d1
Fisnik369 <59198126+Fisnik369@users.noreply.github.com>
2020-03-28 20:01:47 +0100

    Update Kodi.cpp

--- Kodi.cpp
+++ Kodi.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
 
 
-int main(){
+int main(int argc, char*argv[]){
 
+   SetConsoleTitle("ds");
+  
 cout << "Hello";
 
 return 0;
